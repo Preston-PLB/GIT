c3d746ca3cf31b953a5face2e2e59bb180c722a1
Preston Baxter <131111@s020rmc148-23.local>
2018-11-12 12:02:28 -0600

    Initial Commit

---
+++ GIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GIT
+//
+//  Created by Baxter, Preston L on 11/12/18.
+//  Copyright © 2018 Baxter, Preston L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
